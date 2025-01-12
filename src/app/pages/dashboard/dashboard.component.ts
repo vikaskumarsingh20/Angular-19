69c6167642cce6624039814843fba13fc125a672
VikasKumarSingh <vikaskumarsingh0931@gmail.com>
2025-01-13 01:02:02 +0530

    add functionality of logoff

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  // @HostBinding('class') themeClass = 'dark-theme';
+  // toggleTheme(): void {
+  //   debugger;
+  //   console.log("Theme toggled");
+  //   this.themeClass = this.themeClass === 'dark-theme' ? 'light-theme' : 'dark-theme';
+  // }
+  isDarkTheme: boolean = true;
 
+  constructor(private renderer: Renderer2) {}
+
+  toggleTheme(): void {
+    this.isDarkTheme = !this.isDarkTheme;
+    if (this.isDarkTheme) {
+      this.renderer.addClass(document.body, 'dark-theme');
+      this.renderer.removeClass(document.body, 'light-theme');
+    } else {
+      this.renderer.addClass(document.body, 'light-theme');
+      this.renderer.removeClass(document.body, 'dark-theme');
+    }
+  }
 }
