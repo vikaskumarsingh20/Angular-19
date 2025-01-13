b6c1eb8f7cff25d0388d58ff28b9d16f1d5b820d
VikasKumarSingh <vikaskumarsingh0931@gmail.com>
2025-01-13 23:48:15 +0530

    functionality

---
+++ src/app/loader/loader.component.ts
@@ -0,0 +1,12 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  imports: [NgFor],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.css'
+})
+export class LoaderComponent {
+
+}
